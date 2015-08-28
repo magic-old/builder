0b85ece75e6acfad890c269afad84760bf9ec918
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-28 22:03:16 +0200

    deal with config.js

--- src/gulp/tasks/html.js
+++ src/gulp/tasks/html.js
@@ -1,33 +1,16 @@
 export function html(g, conf, p) {
-  const {dirs, server} = conf;
-  const sources = [
-    {
-      src: p.join(dirs.src, dirs.html, dirs.pages, '*.jade'),
-      out: dirs.out,
-    },
-    {
-      src: p.join(dirs.src, dirs.v1, dirs.html, dirs.pages, '*.jade'),
-      out: p.join(dirs.out, dirs.v1),
-    },
-  ];
-
-  const out = dirs.out;
+  const {dirs, server, files, locals} = conf;
 
   const {env, socialAccounts, menuItems, jadeDirs} = conf;
 
-  return () => sources.map(source => g.src(source.src)
-                      .pipe(p.jade({
-                        jade: p.compileJade,
-                        pretty: true,
-                        locals: {
-                          env,
-                          socialAccounts,
-                          menuItems,
-                          dirs: server.dirs,
-                        },
-                      }))
-                      .on('error', p.util.log)
-                      .pipe(g.dest(source.out))
-                      .pipe(p.livereload())
-                );
+  return () => files.html.map(source => g.src(source.src)
+                        .pipe(p.jade({
+                          jade: p.compileJade,
+                          pretty: true,
+                          locals,
+                        }))
+                        .on('error', p.util.log)
+                        .pipe(g.dest(source.out))
+                        .pipe(p.livereload())
+              );
 }
