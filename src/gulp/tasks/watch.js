4e342fd6338d5fa288b2c058297f5421ab372c77
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-28 17:40:46 +0200

    added v1 watch tasks

--- src/gulp/tasks/watch.js
+++ src/gulp/tasks/watch.js
@@ -1,6 +1,6 @@
 export function watch(g, conf, p) {
   const {port} = conf;
-  const {js, css, html, pages, assets} = conf.dirs;
+  const {js, css, html, pages, assets, v1} = conf.dirs;
   const {config, src, appcache} = conf.watch;
 
   const basePath = '/';
@@ -10,11 +10,15 @@ export function watch(g, conf, p) {
     p.livereload.listen({basePath, host});
 
     g.watch(`${src}/${js}/**/*.js`, ['build:js']);
+    g.watch(`${src}/${v1}/${js}/**/*.js`, ['build:js']);
     g.watch(`${src}/${css}/**/*.styl`, ['build:css']);
+    g.watch(`${src}/${v1}/${css}/**/*.styl`, ['build:css']);
     g.watch(`${src}/${html}/**/*.jade`, ['build:html']);
+    g.watch(`${src}/${v1}/${html}/**/*.jade`, ['build:html']);
     g.watch(`${config}/*`, ['build']);
     g.watch('config.js', ['build']);
     g.watch(`${src}/${assets}/**/*`, ['build:copy']);
+    g.watch(`${src}/${v1}/${assets}/**/*`, ['build:copy']);
     g.watch(`${src}/${appcache}`, ['build:appcache']);
   }
 }
