cf0ad97795afca89fb3f23cfee31a1a2aea22d28
Jascha Ehrenreich <jascha@jaeh.at>
2015-09-01 20:13:41 +0200

    use st as server in development

--- src/gulp/tasks/server.js
+++ src/gulp/tasks/server.js
@@ -1,6 +1,20 @@
+import {createServer} from 'http';
+
 export function server(g, conf, p) {
 
-  return () => {
-    require('magicserver');
+  return (done) => {
+    if (conf.env === 'development') {
+      let mount = p.st({
+            path: conf.dirs.out,
+            url: '/',
+            cache: false,
+            index: 'index.html',
+          });
+
+      createServer(mount)
+        .listen(conf.port, done);
+    } else {
+      require('magicserver');
+    }
   }
 }
